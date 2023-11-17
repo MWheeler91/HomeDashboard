a10c42a066ed5979229360e2df7abd0dbba4c222
= <=>
2023-11-16 23:44:29 -0600

    Updated Maint models, seriaizer, views, URLs files

--- homedashboard_django/maintenance/urls.py
+++ homedashboard_django/maintenance/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from catalog import views
+from maintenance import views
 
-app_name = "catelog"
+app_name = "Maintenance"
 
 urlpatterns = [
-    path('get-vehicles', views.GetValues.as_view(), name='getvalues'),
-    path('new-maintenance', views.NewItem.as_view(), name='newitem' ),
-    path('maintenance-list', views.GetAllItems.as_view(), name='itemlist' ),
-    path('delete-maintenance/<int:id>', views.DeleteItem.as_view(), name='deleteitem' ),
-    path('update-maintenance/<int:id>', views.UpdateItem.as_view(), name='updateitem' ),
+    path('get-vehicles', views.GetVehicles.as_view(), name='get-vehicles'),
+    # path('new-maintenance', views.GetVehicles.as_view(), name='newitem' ),
+    path('maintenance-list', views.GetMaintenanceList.as_view(), name='itemlist' ),
+    # path('delete-maintenance/<int:id>', views.GetVehicles.as_view(), name='deleteitem' ),
+    # path('update-maintenance/<int:id>', views.GetVehicles.as_view(), name='updateitem' ),
 ]
