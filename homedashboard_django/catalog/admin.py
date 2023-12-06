145c669eb336db40ae33d505efddb58d1c3046ac
= <=>
2023-12-06 14:02:24 -0600

    Updated models

--- homedashboard_django/catalog/admin.py
+++ homedashboard_django/catalog/admin.py
@@ -5,4 +5,25 @@ from .models import *
 admin.site.register(Room)
 admin.site.register(Category)
 admin.site.register(Condition)
-admin.site.register(Item)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'entered_by',
+        'date_entered',
+        'last_updated_date',
+        'last_updated_time',
+        'last_updated_by'
+    )
+
+    def save_model(self, request, obj, form, change):
+        # If the object is being created (not changed), set both fields
+        if not change:
+            obj.entered_by = request.user
+        # For every save (including updates), set the last_updated_by field
+        obj.last_updated_by = request.user
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Item, ItemAdmin)
