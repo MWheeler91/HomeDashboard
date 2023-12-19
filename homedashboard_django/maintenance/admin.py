a8cd1a5f3f9e6f37ce5ccf0180bd0d50c7170383
= <=>
2023-12-18 18:10:36 -0600

    Created new model

--- homedashboard_django/maintenance/admin.py
+++ homedashboard_django/maintenance/admin.py
@@ -2,8 +2,37 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Vehicle)
-admin.site.register(Maintenance)
+
+
+# default admin class to set read only fields and set auto update saved by
+class DefaultAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'entered_by',
+        'date_entered',
+        'last_updated_date',
+        'last_updated_time',
+        'last_updated_by'
+    )
+
+    def save_model(self, request, obj, form, change):
+        # If the object is being created (not changed), set both fields
+        if not change:
+            obj.entered_by = request.user
+        # For every save (including updates), set the last_updated_by field
+        obj.last_updated_by = request.user
+
+        super().save_model(request, obj, form, change)
+
+
+
+
+
+
+
+
+admin.site.register(Vehicle, DefaultAdmin)
+admin.site.register(Maintenance, DefaultAdmin)
 admin.site.register(MaintenanceFile)
-admin.site.register(Accessory)
+admin.site.register(Accessory, DefaultAdmin)
 admin.site.register(AccessoriesFile)
+admin.site.register(VehicleRegistration, DefaultAdmin)
