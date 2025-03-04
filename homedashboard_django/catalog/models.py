4589363dd9c851d06a68c2bbdc6bde3dae02c88b
mwheeler91 <matthew@mwheeler.dev>
2025-03-03 20:42:56 -0600

    set default for JSON fields

--- homedashboard_django/catalog/models.py
+++ homedashboard_django/catalog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
 from account.models import User
-# Create your models here.
+# Create your models here
 
+def get_json():
+    return { 'Field1': [], 'Field2': [],'Field3': []}
 
 class Category(models.Model):
     item_category = models.CharField(max_length=200, unique=True)
@@ -37,7 +39,7 @@ class Item(models.Model):
     model_number = models.CharField(max_length=50, blank=True, null=True)
     asset_tag = models.CharField(max_length=50, blank=True, null=True)
     has_accessories = models.BooleanField(default=False)
-    extra = models.JSONField(default=dict, blank=True, null=True)
+    extra = models.JSONField(default=get_json, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     entered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='items')
     date_entered = models.DateField(default=datetime.now)
@@ -68,7 +70,7 @@ class ItemAccessories(models.Model):
     value_now = models.DecimalField(max_digits=100, decimal_places=2, blank=True, null=True)
     serial_number = models.CharField(max_length=50, blank=True, null=True)
     model_number = models.CharField(max_length=50, blank=True, null=True)
-    extra = models.JSONField(default=dict, blank=True, null=True)
+    extra = models.JSONField(default=get_json, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     entered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='items_Accessories')
     date_entered = models.DateField(default=datetime.now)
