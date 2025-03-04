e99dd0a18af4f0757303b0c0f38e9a3ca08ec637
mwheeler91 <matthew@mwheeler.dev>
2025-03-03 19:26:50 -0600

    changes.... again

--- homedashboard_django/catalog/models.py
+++ homedashboard_django/catalog/models.py
@@ -24,21 +24,6 @@ class Condition(models.Model):
     def __str__(self):
         return "{}".format(self.condition)
 
-class ItemAccessories(models.Model):
-    Item = models.ForeignKey(Item, limit_choices_to={'has_accessories':True}, on_delete=models.SET_NULL, blank=True, null=True)
-    purchase_price = models.DecimalField(max_digits=100, decimal_places=2, blank=True, null=True)
-    purchase_date = models.DateField(blank=True, null=True)
-    value_now = models.DecimalField(max_digits=100, decimal_places=2, blank=True, null=True)
-    serial_number = models.CharField(max_length=50, blank=True, null=True)
-    model_number = models.CharField(max_length=50, blank=True, null=True)
-    extra = models.JSONField(default=dict, blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    entered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='items_Accessories')
-    date_entered = models.DateField(default=datetime.now)
-    last_updated_date = models.DateField(auto_now=True, blank=True, null=True)
-    last_updated_time = models.TimeField(auto_now=True,  blank=True, null=True)
-    last_updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
 class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200, blank=True, null=True)
@@ -74,3 +59,19 @@ class Item(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         ItemAccessories.objects.filter(Item=self).update(is_active=self.is_active)
+
+
+class ItemAccessories(models.Model):
+    Item = models.ForeignKey(Item, limit_choices_to={'has_accessories':True}, on_delete=models.SET_NULL, blank=True, null=True)
+    purchase_price = models.DecimalField(max_digits=100, decimal_places=2, blank=True, null=True)
+    purchase_date = models.DateField(blank=True, null=True)
+    value_now = models.DecimalField(max_digits=100, decimal_places=2, blank=True, null=True)
+    serial_number = models.CharField(max_length=50, blank=True, null=True)
+    model_number = models.CharField(max_length=50, blank=True, null=True)
+    extra = models.JSONField(default=dict, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    entered_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='items_Accessories')
+    date_entered = models.DateField(default=datetime.now)
+    last_updated_date = models.DateField(auto_now=True, blank=True, null=True)
+    last_updated_time = models.TimeField(auto_now=True,  blank=True, null=True)
+    last_updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
